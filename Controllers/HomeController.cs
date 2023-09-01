1312abbfc027d4dcc183bbb4bbeb2279ce0e3900
Wescley Batista <wescley.batista1@aluno.unip.br>
2023-09-01 11:48:20 -0300

    criação de um controlador para o login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using TCC.Models;
+using TCC.ViewModels; // Certifique-se de que o namespace esteja correto e inclua sua ViewModel de login
 
 namespace TCC.Controllers
 {
@@ -23,9 +25,35 @@ namespace TCC.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Login()
         {
-            return View();
+            return View(new LoginViewModel()); // Certifique-se de que a ViewModel esteja corretamente definida
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aqui você deve adicionar a lógica para verificar as credenciais do usuário
+                // Substitua este exemplo por sua própria lógica de autenticação
+
+                if (model.UserName == "usuario" && model.Password == "senha")
+                {
+                    // Autenticação bem-sucedida
+                    // Você pode adicionar lógica para criar uma sessão de usuário ou definir um cookie de autenticação aqui
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Credenciais inválidas");
+                }
+            }
+
+            // Se a autenticação falhar ou se o modelo não for válido, retorne a mesma página de login com erros
+            return View(model);
         }
 
         public IActionResult Ranking()
