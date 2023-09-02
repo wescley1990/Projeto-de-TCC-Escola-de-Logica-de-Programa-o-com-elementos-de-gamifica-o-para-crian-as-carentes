f089b0bf8bea60b7682588daadefcfb1084c1837
Ygor Lacerda <ygor.l@financas360.com.br>
2023-09-02 12:03:57 -0300

    ajustando error view model

--- src/TCC.UI.Web/Controllers/HomeController.cs
+++ src/TCC.UI.Web/Controllers/HomeController.cs
@@ -23,10 +23,35 @@ namespace TCC.UI.Web.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("error/{id:length(3,3)}")]
+        public IActionResult Errors(int id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var modelErro = new ErrorViewModel();
+
+            if (id == 500)
+            {
+                modelErro.Message = "An error has occurred! Please try again later or contact our support.";
+                modelErro.Title = "An error has occurred!";
+                modelErro.ErrorCode = id;
+            }
+            else if (id == 404)
+            {
+                modelErro.Message = "The page you are looking for does not exist! <br /> If you have any questions please contact our support";
+                modelErro.Title = "Oops! Page not found.";
+                modelErro.ErrorCode = id;
+            }
+            else if (id == 403)
+            {
+                modelErro.Message = "You are not allowed to do this.";
+                modelErro.Title = "Access Denied";
+                modelErro.ErrorCode = id;
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+
+            return View("Error", modelErro);
         }
     }
 }
--- src/TCC.UI.Web/Models/ErrorViewModel.cs
+++ src/TCC.UI.Web/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@ namespace TCC.UI.Web.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int ErrorCode { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
     }
 }
