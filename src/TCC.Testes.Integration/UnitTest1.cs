a25ac130ef0d6a62f76f1f1938642ff53cb360b7
Ygor Lacerda <ygor.l@financas360.com.br>
2023-09-02 13:06:13 -0300

    adicionando projetos de teste.

---
+++ src/TCC.Testes.Integration/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TCC.Testes.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/TCC.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TCC.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
