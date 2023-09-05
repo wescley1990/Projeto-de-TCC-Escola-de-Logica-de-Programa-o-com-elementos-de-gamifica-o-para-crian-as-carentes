8585abd90dc7fd516588e71b0ac7408a9563af25
Ygor Lacerda <ygor.l@financas360.com.br>
2023-09-05 17:07:25 -0300

    adicionando controllers e roteamento

---
+++ src/TCC.UI.Web/Controllers/LojaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCC.UI.Web.Controllers
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
