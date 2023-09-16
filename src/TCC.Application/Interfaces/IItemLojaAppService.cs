c105fdb4ba05f642c16ac4f88e4ab97e7b40c8bb
Ygor Lacerda <ygor.l@financas360.com.br>
2023-09-16 10:17:46 -0300

    adicionando loja service.

---
+++ src/TCC.Application/Interfaces/IItemLojaAppService.cs
@@ -0,0 +1,8 @@
+﻿using TCC.Application.ViewModels;
+
+namespace TCC.Application.Interfaces;
+
+public interface IItemLojaAppService : IDisposable
+{
+    Task<IEnumerable<ItemLojaViewModel>> GetAll();
+}
---
+++ src/TCC.Application/Services/ItemLojaAppService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using TCC.Application.Interfaces;
+using TCC.Application.ViewModels;
+using TCC.Domain.Interfaces;
+
+namespace TCC.Application.Services;
+
+public class ItemLojaAppService : IItemLojaAppService
+{
+    private readonly IItemLojaRepository _itemLojaRepository;
+    private readonly IMapper _mapper;
+
+    public ItemLojaAppService(
+        IItemLojaRepository itemLojaRepository,
+        IMapper mapper
+    )
+    {
+        _itemLojaRepository = itemLojaRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<IEnumerable<ItemLojaViewModel>> GetAll()
+    {
+        return _mapper.Map<IEnumerable<ItemLojaViewModel>>(await _itemLojaRepository.GetAll());
+    }
+    
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
