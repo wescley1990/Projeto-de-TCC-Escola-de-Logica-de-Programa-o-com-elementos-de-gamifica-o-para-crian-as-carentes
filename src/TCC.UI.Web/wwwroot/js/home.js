58af582ecbff239c878d6bb20ba435bed11d77cc
Wescley Batista <wescley.batista1@aluno.unip.br>
2023-09-27 12:25:44 -0300

    inclusao de novos parallax e textos

--- src/TCC.UI.Web/wwwroot/js/home.js
+++ src/TCC.UI.Web/wwwroot/js/home.js
@@ -1,10 +1,14 @@
 const sr = ScrollReveal({ reset: true })
 
 sr.reveal('.imagem1', {
+    duration: 2000
+});
+
+sr.reveal('.apresentacao h2', {
     duration: 3000
 });
 
 
 sr.reveal('.service-item', {
-    duration: 2000
+    duration: 3000
 });
