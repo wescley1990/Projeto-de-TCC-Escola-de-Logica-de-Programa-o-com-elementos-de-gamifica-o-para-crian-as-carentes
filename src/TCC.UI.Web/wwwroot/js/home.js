4c6a9f7bc68c20bdf4e4bea79af830d33ac09b38
Wescley Batista <wescley.batista1@aluno.unip.br>
2023-09-29 13:49:23 -0300

    resolvido problema do carrosel

--- src/TCC.UI.Web/wwwroot/js/home.js
+++ src/TCC.UI.Web/wwwroot/js/home.js
@@ -1,8 +1,8 @@
 const sr = ScrollReveal({ reset: true })
 
-sr.reveal('.imagem1', {
-    duration: 2000
-});
+////sr.reveal('.imagem1', {
+////    duration: 2000
+////});
 
 sr.reveal('.apresentacao h2', {
     duration: 3000
