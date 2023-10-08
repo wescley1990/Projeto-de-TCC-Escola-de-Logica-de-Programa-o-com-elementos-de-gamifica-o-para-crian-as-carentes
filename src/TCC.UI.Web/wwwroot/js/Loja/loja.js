c1769e608d30ea5be1df34d5f77f5783cae6fab5
Gabriel Gomes <gabriel.macedo90@hotmail.com>
2023-10-08 17:43:45 -0300

    JS LOJA

---
+++ src/TCC.UI.Web/wwwroot/js/Loja/loja.js
@@ -0,0 +1,16 @@
+﻿function verificarAlturaItens() {
+    var tabela = document.querySelector(".item-table");
+    var alturaItens = tabela.scrollHeight;
+    var alturaVisivel = tabela.clientHeight;
+
+    if (alturaItens > alturaVisivel) {
+      
+        tabela.scrollTop = alturaItens - alturaVisivel;
+    }
+}
+
+
+window.addEventListener("load", verificarAlturaItens);
+
+
+window.addEventListener("resize", verificarAlturaItens);
