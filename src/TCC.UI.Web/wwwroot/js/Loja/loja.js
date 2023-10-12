83249bc16018f68d192573e5c4ecc9eaad35ef44
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-12 11:43:17 -0300

    ajuste js

--- src/TCC.UI.Web/wwwroot/js/Loja/loja.js
+++ src/TCC.UI.Web/wwwroot/js/Loja/loja.js
@@ -1,9 +1,5 @@
 ﻿const comprar1 = document.getElementById('comprar');
-const comprar2 = document.getElementById('comprar2');
-const comprar3 = document.getElementById('comprar3');
-const comprar4 = document.getElementById('comprar4');
-const comprar5 = document.getElementById('comprar5');
-const comprar6 = document.getElementById('comprar6');
+
 const modal = document.getElementById('myModal');
 const modal2 = document.getElementById('myModal2');
 const confirmButton = document.getElementById('confirmButton');
@@ -15,66 +11,6 @@ comprar1.addEventListener('click', function (e) {
 });
 
 comprar1.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar2.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar2.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar2.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar2.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar3.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar3.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar4.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar4.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar5.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar5.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal2.style.display = 'flex';
-});
-
-comprar6.addEventListener('click', function (e) {
-    e.preventDefault();
-    modal.style.display = 'flex';
-});
-
-comprar6.addEventListener('click', function (e) {
     e.preventDefault();
     modal2.style.display = 'flex';
 });
